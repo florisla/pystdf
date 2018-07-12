dbd40fb4f3f2f274a43a3ad38797dde47c18d6cc
Floris Lambrechts <florisla@gmail.com>
2018-07-12 14:45:34 +0200

    Port setup.py to setuptools
    Main advantage: you can now also build wheel packages.
    
    Based on https://github.com/kennethreitz/setup.py.
    
    Features:
    - read version directly from __version__.py
    - read long description directly from README.md
    - render description nicely formatted in PyPI
    - install with 'extras' ('dataframe' or 'xlsx') to get additional
    dependencies.
      E.g. pip install -f [folder-with-wheel-package] pystdf[xlsx]

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
- #!/usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # PySTDF - The Pythonic STDF Parser
 # Copyright (C) 2006 Casey Marshall
@@ -7,57 +8,99 @@
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
 
+import io
+import os
 import sys
-from distutils.core import setup
+from shutil import rmtree
 
-setup(name='pystdf',
-    version='1.3.2',
-    description="Python module for working with STDF files",
-    long_description="""
-PySTDF is a Python module that makes it easy to work with STDF (Teradyne's Standard Test Data Format). STDF is a commonly used file format in semiconductor test -- automated test equipment (ATE) from such vendors as Teradyne, Verigy, LTX, Credence, and others support this format.
+from setuptools import find_packages, setup, Command
 
-PySTDF provides event-based stream parsing of STDF version 4, along with indexers that can help you rearrange the data into a more useful tabular form, as well as generate missing summary records or new types of derivative records.
+NAME = 'pystdf'
+DESCRIPTION = 'Python module for working with STDF files'
+URL = 'https://github.com/cmars/pystdf'
+EMAIL = 'casey.marshall@gmail.com'
+AUTHOR = 'Casey Marshall'
+REQUIRES_PYTHON = '>=3.4.0'
+VERSION = None
 
-The parser architecture is very flexible and can easily be extended to support STDF version 3 as well as custom record types.
+REQUIRED = [
+]
 
-Potential applications of PySTDF include:
-* Debugging a vendor's STDF implementation
-* Straight conversion to ASCII-readable form
-* Repairing STDF files
-* Developing an application that leverages STDF
-  - Conversion to tabular form for statistical analysis tools
-  - Loading data into a relational database
+EXTRAS = {
+    'dataframe': ['pandas'],
+    'xlsx': ['pandas', 'openpyxl'],
+}
 
-PySTDF is released under a GPL license. Applications developed with PySTDF can only be released with a GPL-compatible license. Commercial applications can purchase an alternate license agreement for closed-source distribution.
-""",
-    author='Casey Marshall',
-    author_email='casey.marshall@gmail.com',
-    url='http://code.google.com/p/pystdf/',
-    packages=['pystdf','pystdf.explorer'],
-    scripts=['pystdf/script/slice.py', 'pystdf/script/count.py', 'pystdf/script/totext.py',
-             'pystdf/script/toexcel.py', 'pystdf/script/toxml.py'],
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['VERSION'] = VERSION
+
+
+setup(
+    name=NAME,
+    version='.'.join(str(num) for num in about['VERSION']),
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages=find_packages(exclude=('tests',)),
+    entry_points={
+        'console_scripts': [
+            'stdf2text=pystdf.script.totext:main',
+            'stdf2excel=pystdf.script.toexcel:main',
+            'stdf2xml=pystdf.script.toxml:main',
+            'stdfcount=pystdf.script.count:main',
+            'stdfslice=pystdf.script.slice:main',
+        ],
+    },
+    install_requires=REQUIRED,
+    extras_require=EXTRAS,
+    include_package_data=True,
+    license='MIT',
     classifiers=[
-      'Development Status :: 4 - Beta',
-      'Environment :: Console',
-      'License :: Free for non-commercial use',
-      'License :: OSI Approved :: GNU General Public License (GPL)',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python',
-      'Intended Audience :: Manufacturing',
-      'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
-      'Topic :: Utilities',
-      'Topic :: Software Development :: Libraries :: Python Modules',
-      'Topic :: Software Development :: Pre-processors',
-      ],
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: Free for non-commercial use',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)'
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy'
+        'Operating System :: OS Independent',
+        'Intended Audience :: Manufacturing',
+        'Topic :: Scientific/Engineering :: Electronic Design Automation (EDA)',
+        'Topic :: Utilities',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
 )
+
